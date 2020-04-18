babd4163eea2374f5e1e613585771d312cf62459
Tobey Blaber <45315526+toberandober@users.noreply.github.com>
2020-04-18 16:40:46 +0100

    HarmonyPatcher is now internal.

--- VersionChecker/HarmonyPatcher.cs
+++ VersionChecker/HarmonyPatcher.cs
@@ -1,11 +1,7 @@
-﻿using QModManager.API.ModLoading;
-
-namespace Straitjacket.Utility
+﻿namespace Straitjacket.Utility
 {
-    [QModCore]
-    public class HarmonyPatcher
+    internal class HarmonyPatcher
     {
-        [QModPatch]
         public static void ApplyPatches() {
             VersionChecker.Check<ModJson>(
                 "https://github.com/tobeyStraitjacket/VersionChecker/raw/master/VersionChecker/mod.json"
