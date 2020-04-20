1d741250ea20837c483d2e3ecf5a5ef23b5273d2
Tobey Blaber <45315526+toberandober@users.noreply.github.com>
2020-04-20 21:53:07 +0100

    Implementations of VersionFormat for Assembly Versioning and Semantic Versioning.

---
+++ VersionChecker/VersionFormats/AssemblyVersion.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Straitjacket.Utility.VersionFormats
+{
+    /// <summary>
+    /// Represents a version number formatted under Assembly Versioning.
+    /// </summary>
+    public class AssemblyVersion : VersionFormat
+    {
+        /// <summary>
+        /// Initiailises a new <see cref="AssemblyVersion"/> instance.
+        /// </summary>
+        /// <seealso cref="AssemblyVersion(string)"/>
+        /// <seealso cref="AssemblyVersion(Version)"/>
+        public AssemblyVersion() { }
+        
+        /// <summary>
+        /// Initailises a new <see cref="AssemblyVersion"/> instance using the specified <see cref="string"/>.
+        /// </summary>
+        /// <param name="version">A string representing an Assembly Versioning version number.</param>
+        /// <seealso cref="AssemblyVersion(Version)"/>
+        /// <seealso cref="AssemblyVersion()"/>
+        public AssemblyVersion(string version) => Version = new Version(version).ToStringParsed();
+        
+        /// <summary>
+        /// Initialises a new <see cref="AssemblyVersion"/> instance using the specified <see cref="AssemblyVersion"/>.
+        /// </summary>
+        /// <param name="version">A <see cref="Version"/> representing an Assembly Versioning version number.</param>
+        /// <seealso cref="AssemblyVersion(string)"/>
+        /// <seealso cref="AssemblyVersion()"/>
+        public AssemblyVersion(Version version) => Version = version.ToStringParsed();
+        
+        /// <summary>
+        /// Compares the current <see cref="AssemblyVersion"/> to a specified <see cref="VersionFormat"/> and returns an indicataion of their relative values.
+        /// </summary>
+        /// <param name="other">A <see cref="VersionFormat"/> representing an Assembly Versioning version number.</param>
+        /// <returns>An <see cref="int"/> indicating the relative values of the two Assembly Versioning version numbers.</returns>
+        public override int CompareTo(VersionFormat other) => new Version(Version).CompareTo(new Version(other.Version));
+    }
+}
---
+++ VersionChecker/VersionFormats/Semver.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Semver;
+
+namespace Straitjacket.Utility.VersionFormats
+{
+    /// <summary>
+    /// Represents a version number formatted under Semantic Versioning (SemVer).
+    /// </summary>
+    public class SemVer : VersionFormat
+    {
+        /// <summary>
+        /// Initialises a new <see cref="SemVer"/> instance.
+        /// </summary>
+        /// <seealso cref="SemVer(string)"/>
+        /// <seealso cref="SemVer(SemVersion)"/>
+        public SemVer() { }
+
+        /// <summary>
+        /// Initialises a new <see cref="SemVer"/> instance using the specified <see cref="string"/>.
+        /// </summary>
+        /// <param name="version">A string representing a SemVer version number.</param>
+        /// <seealso cref="SemVer(SemVersion)"/>
+        /// <seealso cref="SemVer()"/>
+        public SemVer(string version) => Version = SemVersion.Parse(version).ToString();
+
+        /// <summary>
+        /// Initialises a new <see cref="SemVer"/> instance using the specified <see cref="SemVersion"/>.
+        /// </summary>
+        /// <param name="version">A <see cref="SemVersion"/> representing a SemVer version number.</param>
+        /// <seealso cref="SemVer(string)"/>
+        /// <seealso cref="SemVer()"/>
+        public SemVer(SemVersion version) => Version = version.ToString();
+
+        /// <summary>
+        /// Compares the current <see cref="SemVer"/> to a specified <see cref="VersionFormat"/> and returns an indication of their relative values.
+        /// </summary>
+        /// <param name="other">A <see cref="VersionFormat"/> representing a SemVer version number.</param>
+        /// <returns>An <see cref="int"/> indicating the relative values of the two SemVer version numbers.</returns>
+        public override int CompareTo(VersionFormat other) => SemVersion.Parse(Version).CompareTo(SemVersion.Parse(other.Version));
+    }
+}
