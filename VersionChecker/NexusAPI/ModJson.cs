af211ca4f1590d954d22cb557edcd4accec29905
Tobey Blaber <tobey@straitjacket.software>
2020-10-05 16:59:08 +0100

    Add JSON parser support for nexus mods API and VersionChecker API

---
+++ VersionChecker/NexusAPI/ModJson.cs
@@ -0,0 +1,29 @@
+﻿#if SUBNAUTICA
+using Oculus.Newtonsoft.Json;
+#elif BELOWZERO
+using Newtonsoft.Json;
+#endif
+
+namespace Straitjacket.Utility.NexusAPI
+{
+    internal class ModJson
+    {
+        [JsonRequired, JsonProperty(PropertyName = "version")]
+        public string Version { get; set; }
+
+        [JsonRequired, JsonProperty(PropertyName = "mod_id")]
+        public int ModId { get; set; }
+
+        [JsonRequired, JsonProperty(PropertyName = "game_id")]
+        public int GameId { get; set; }
+
+        [JsonRequired, JsonProperty(PropertyName = "name")]
+        public string Name { get; set; }
+
+        [JsonRequired, JsonProperty(PropertyName = "domain_name")]
+        public string DomainName { get; set; }
+
+        [JsonRequired, JsonProperty(PropertyName = "available")]
+        public bool Available { get; set; }
+    }
+}
